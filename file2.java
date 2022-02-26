94f8e05ce5b2b6cb0ec54ae9129211008d4db4e2
naina verma <nainaverma@gmail.com>
2022-02-26 09:45:34 +0000

    reverting my soft reset

--- file2.java
+++ file2.java
@@ -6,4 +6,3 @@ ssh-w4ELvxt8gz1O
 systemd-private-5ade486f6fc5418aab9e58f642e45cfa-rtkit-daemon.service-KYIDzj
 systemd-private-5ade486f6fc5418aab9e58f642e45cfa-systemd-timesyncd.service-6lxV4c
 winstone37984941814972179.jar
-making some more changes for the commit
