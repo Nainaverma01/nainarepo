660d021874cb0692bb3817a344564d65aa0b0147
naina verma <nainaverma@gmail.com>
2022-02-26 08:20:52 +0000

    my first commit

---
+++ file1.java
@@ -0,0 +1,9 @@
+total 3248
+-rw-rw-r-- 1 nainaverma456gm nainaverma456gm       6 Feb 26 05:53 check_idle
+drwxr-xr-x 2 jenkins         jenkins            4096 Feb 26 05:26 hsperfdata_jenkins
+drwxr-xr-x 2 root            root               4096 Feb 26 05:27 hsperfdata_root
+drwx------ 2 jenkins         jenkins            4096 Feb 26 05:26 jetty-0_0_0_0-8080-war-_-any-4513804425347954016
+drwx------ 2 nainaverma456gm nainaverma456gm    4096 Feb 26 05:36 ssh-w4ELvxt8gz1O
+drwx------ 3 root            root               4096 Feb 26 05:26 systemd-private-5ade486f6fc5418aab9e58f642e45cfa-rtkit-daemon.service-KYIDzj
+drwx------ 3 root            root               4096 Feb 26 05:26 systemd-private-5ade486f6fc5418aab9e58f642e45cfa-systemd-timesyncd.service-6lxV4c
+-rw-r--r-- 1 jenkins         jenkins         3293397 Feb 26 05:26 winstone37984941814972179.jar
---
+++ file2.java
@@ -0,0 +1,8 @@
+check_idle
+hsperfdata_jenkins
+hsperfdata_root
+jetty-0_0_0_0-8080-war-_-any-4513804425347954016
+ssh-w4ELvxt8gz1O
+systemd-private-5ade486f6fc5418aab9e58f642e45cfa-rtkit-daemon.service-KYIDzj
+systemd-private-5ade486f6fc5418aab9e58f642e45cfa-systemd-timesyncd.service-6lxV4c
+winstone37984941814972179.jar
